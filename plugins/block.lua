c75d2fb25dde12dffa4670b4609963b6a8b85f50
nicesource <sourceeme@gmail.com>
2016-08-31 17:50:38 +0430

    Update block.lua

--- plugins/block.lua
+++ plugins/block.lua
@@ -1,41 +1,31 @@
---[[
-
-     **************************
-     *  BlackPlus Plugins...  *
-     *                        *
-     *     By @MehdiHS        *
-     *                        *
-     *  Channel > @Black_Ch   *
-     **************************
-	 
-]]
+do
+local function block_user_callback(cb_extra, success, result)
+  local receiver = cb_extra.receiver
+  local user = 'user#id'..result.id
+  if success == 0 then
+    return send_large_msg(receiver, "I cant block user.")
+  end
+  block_user(user, cb_ok, false)
+end
+end
 local function run(msg, matches)
-    if matches[1]:lower() == '+' and is_sudo(msg) then
-        block_user("user#id"..matches[2],ok_cb,false)
-        return "User blocked"
+ if msg.to.type == 'chat' then
+    local user = 'chat#id'..msg.to.id
+ local user = matches[2]
+  if matches[1] == "user" then
+      user = 'user#id'..user
+      block_user(user, callback, false)
     end
-    if matches[1]:lower() == '-' and is_sudo(msg) then
-	    unblock_user("user#id"..matches[2],ok_cb,false)
-        return "User unblocked"
-    end
+    if not is_sudo(msg) then
+    return "sicktir baw only sudo "
+  end
+    return "User Has Been Blocked"
+  end
+end
+
 return {
   patterns = {
-	"^[Bb]lock (+) (%d+)$",
-	"^[Bb]lock (-) (%d+)$",
-	"^[#!/][Bb]lock (+) (%d+)$",
-	"^[#!/][Bb]lock (-) (%d+)$",
+    "^!block (user) (%d+)$",
   },
   run = run,
 }
-end
---[[
-
-     **************************
-     *  BlackPlus Plugins...  *
-     *                        *
-     *     By @MehdiHS        *
-     *                        *
-     *  Channel > @Black_Ch   *
-     **************************
-	 
-]]
